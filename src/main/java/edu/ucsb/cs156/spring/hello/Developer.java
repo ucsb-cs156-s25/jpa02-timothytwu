40922f7c6706e7b44310e4ed3954ec337331559c
Timothy Wu <timothytwu@ucsb.edu>
2025-04-03 22:25:57 -0700

    updated developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Timothy";
     }
 
     /**
@@ -29,8 +29,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "timothytwu";
     }
 
     /**
@@ -39,14 +38,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-13");
+        team.addMember("Timothy");
+        team.addMember("Kenneth");
+        team.addMember("Nikunj");
+        team.addMember("Tuan");
+        team.addMember("Conner");
+        team.addMember("Cheng");
         return team;
     }
 }
