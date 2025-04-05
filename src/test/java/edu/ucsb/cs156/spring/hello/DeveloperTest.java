71e977198d8f4768ba1799db808144eeb155ff52
Timothy Wu <timothytwu@ucsb.edu>
2025-04-04 17:29:41 -0700

    full test coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,7 +25,21 @@ public class DeveloperTest {
         assertEquals("Timothy", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+
+    @Test
+    public void getCorrectGitID() {
+        assertEquals("timothytwu", Developer.getGithubId());
+    }
+    @Test
+    public void getTeam_correctName() {
+        Team t = Developer.getTeam();
+        assertEquals("s25-13", t.getName());
+        assertTrue(t.getMembers().contains("Timothy"),"Team doesn't contain Timothy");
+        assertTrue(t.getMembers().contains("Kenneth"),"Team doesn't contain Kenneth");
+        assertTrue(t.getMembers().contains("Nikunj"),"Team doesn't contain Nikunj");
+        assertTrue(t.getMembers().contains("Tuan"),"Team doesn't contain Tuan");
+        assertTrue(t.getMembers().contains("Conner"),"Team doesn't contain Conner");
+        assertTrue(t.getMembers().contains("Cheng"),"Team doesn't contain Cheng");
+    }
 
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,4 +23,55 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void checkEqualObj() {
+        Team team2 = new Team("test-team");
+        assert(team2.equals(team2));
+    }
+
+    @Test
+    public void checkNotEqualObj() {
+        Team team2 = new Team("test-team2");
+        Team team3 = new Team("test-team3");
+        assert(!team2.equals(team3));
+    }
+    @Test
+    public void notTeamType() {
+        String notTeam = "not a team";
+        assert(!team.equals(notTeam));
+    }
+    @Test
+    public void TeamToString() {
+        Team team = new Team("test-team");  
+        String teamString = team.toString();
+        assert(teamString.equals("Team(name=test-team, members=[])"));
+    }
+    @Test
+    public void testHashCode() {
+        Team team2 = new Team("test-team");
+        Team team3 = new Team("test-team3");
+        assert(team.hashCode() == team2.hashCode());
+        assert(!(team2.hashCode() == team3.hashCode()));
+    }
+
+    @Test
+    public void testMemberHash(){
+        Team team2 = new Team("test-team");
+        team2.addMember("Timothy");
+        
+        Team team3 = new Team("test-team2");
+        team3.addMember("Timothy2");
+
+        assert(!(team2.hashCode() == team3.hashCode()));
+       
+    }
+    @Test
+    public void testBitOr(){
+        Team team2 = new Team("test-team");
+        team2.addMember("Timothy");
+        
+        int hashCheck = team2.name.hashCode() | team2.members.hashCode();
+        assert(hashCheck == team2.hashCode());
+    }
+
 }
